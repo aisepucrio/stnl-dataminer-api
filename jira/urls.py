7cc7aaad2a78aa43bb229888b3a92b75f8554d3a
GabrielMM47 <gabrielmmendes@outlook.com>
2024-09-02 17:08:18 -0300

    feat: Criação das urls referentes às views de mineração de consulta.
    JIRA: [CSTONE-271][CSTONE-291]

--- jira/urls.py
+++ jira/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import JiraIssueCollectView, IssueListView, IssueDetailView
+from .views import FetchIssueTypesView, JiraIssueCollectView, IssueListView, IssueDetailView, IssueDeleteView
 
 urlpatterns = [
-    path('issues/collect/', JiraIssueCollectView.as_view(), name='issue-collect'),
-    path('issues/', IssueListView.as_view(), name='issue-list'),
-    path('issues/<int:id>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('jira/issues/types/', FetchIssueTypesView.as_view(), name='fetch-issue-types'),
+    path('/jira/issues/collect/', JiraIssueCollectView.as_view(), name='issue-collect'),
+    path('/jira/issues/', IssueListView.as_view(), name='issue-list'),
+    path('/jira/issues/<int:id>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('/jira/issues/<int:id>/delete/', IssueDeleteView.as_view(), name='issue-delete')
 ]
