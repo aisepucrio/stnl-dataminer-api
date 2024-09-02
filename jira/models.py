9bdafddc42aa8c7e0cd83ba8ddedf4ce7a50eb3b
GabrielMM47 <gabrielmmendes@outlook.com>
2024-09-02 17:07:06 -0300

    ref: Remoção da classe de credenciais do Jira, de modo que as credenciais serão recebidas na requisição.
    JIRA: [CSTONE-271][CSTONE-291]

--- jira/models.py
+++ jira/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class JiraCredentials(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    jira_email = models.EmailField()
-    jira_api_token = models.CharField(max_length=100)
+class JiraIssueType(models.Model):
+    domain = models.CharField(max_length=255)
+    project_key = models.CharField(max_length=100)
+    issue_type_id = models.CharField(max_length=100)
+    issue_type_name = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('domain', 'project_key', 'issue_type_id')
 
     def __str__(self):
-        return f"Credentials for {self.user.username}"
+        return f"{self.domain} - {self.project_key}: {self.issue_type_name}"
 
 class JiraIssue(models.Model):
     issue_id = models.CharField(max_length=100, unique=True)
@@ -23,7 +26,7 @@ class JiraIssue(models.Model):
     creator = models.CharField(max_length=100)
     assignee = models.CharField(max_length=100, null=True, blank=True)
     reporter = models.CharField(max_length=100, null=True, blank=True)
-    custom_fields = models.JSONField(null=True, blank=True)
+    all_fields = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.key} - {self.summary}"
