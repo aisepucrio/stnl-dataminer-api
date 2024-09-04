3132b5bac88fb657dcb3bc04d4fb1c2f37c24538
DiogoMarassi <dmaraassi@gmail.com>
2024-09-04 08:12:52 -0300

    add comments

--- .history/github/views_20240903151359.py
+++ .history/github/views_20240903151359.py
@@ -3,7 +3,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-#from github.miner import GitHubMiner
+from github.miner import GitHubMiner
 from jobs.tasks import fetch_commits, fetch_issues, fetch_pull_requests, fetch_branches
 
 class GitHubCommitViewSet(viewsets.ViewSet):
