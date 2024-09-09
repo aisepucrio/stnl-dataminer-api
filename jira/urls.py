ae9e84e0c15638580da0557a455d865f7371879c
GabrielMM47 <gabrielmmendes@outlook.com>
2024-09-09 09:42:45 -0300

    Adicionando as urls referenteS ao Jira.
    JIRA: [CSTONE-271][CSTONE-294]

--- jira/urls.py
+++ jira/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import FetchIssueTypesView, JiraIssueCollectView, IssueListView, IssueDetailView, IssueDeleteView
 
 urlpatterns = [
-    path('jira/issues/types/', FetchIssueTypesView.as_view(), name='fetch-issue-types'),
-    path('/jira/issues/collect/', JiraIssueCollectView.as_view(), name='issue-collect'),
-    path('/jira/issues/', IssueListView.as_view(), name='issue-list'),
-    path('/jira/issues/<int:id>/', IssueDetailView.as_view(), name='issue-detail'),
-    path('/jira/issues/<int:id>/delete/', IssueDeleteView.as_view(), name='issue-delete')
+    path('issues/types/', FetchIssueTypesView.as_view(), name='fetch-issuetypes'),
+    path('issues/collect/', JiraIssueCollectView.as_view(), name='issue-collect'),
+    path('issues/', IssueListView.as_view(), name='issue-list'),
+    path('issues/<int:issue_id>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('issues/<int:issue_id>/delete/', IssueDeleteView.as_view(), name='issue-delete')
 ]
