f5d4479dc96afee77ef6a6f44ed099baa42a8a0c
GabrielMM47 <gabrielmmendes@outlook.com>
2024-09-18 13:52:18 -0300

    test: Adicionando os testes para os endpoints relacionados ao issuetypes.
    JIRA: [CSTONE-319][CSTONE-331]

--- jira/tests.py
+++ jira/tests.py
@@ -24,21 +24,6 @@ def test_collect_issues(jira_domain, project_key, jira_email, jira_api_token, is
     else:
         print("Failed to start issues collection", response.text)
 
-# Função para testar o endpoint de tipos de issues
-def test_fetch_issue_types(jira_domain, jira_email, jira_api_token):
-    url = f"{BASE_URL}/issues/types/"
-    data = {
-        "jira_domain": jira_domain,
-        "jira_email": jira_email,
-        "jira_api_token": jira_api_token
-    }
-    response = requests.post(url, json=data)
-    print(f"Fetch issue types status code: {response.status_code}")
-    if response.status_code == 200:
-        print("Issuetypes data:", response.json())
-    else:
-        print("Failed to retrieve issuetypes", response.json())
-
 def test_list_issues():
     url = f"{BASE_URL}/issues/"
     response = requests.get(url)
@@ -71,6 +56,52 @@ def test_delete_issue(issue_id):
     else:
         print(f"Failed to delete issue {issue_id}. Response: {response.text}")
 
+# Função para testar o endpoint de tipos de issues
+def test_collect_issue_types(jira_domain, jira_email, jira_api_token):
+    url = f"{BASE_URL}/issuetypes/collect/"
+    data = {
+        "jira_domain": jira_domain,
+        "jira_email": jira_email,
+        "jira_api_token": jira_api_token
+    }
+    response = requests.post(url, json=data)
+    print(f"Fetch issue types status code: {response.status_code}")
+    if response.status_code == 200:
+        print("Issuetypes data:", response.json())
+    else:
+        print(f"Failed to retrieve issuetypes. Status code: {response.status_code}. Response: {response.text}")
+
+def test_list_issue_types():
+    url = f"{BASE_URL}/issuetypes/"
+    response = requests.get(url)
+    print(f"Issue types list status code: {response.status_code}")
+    if response.status_code == 200:
+        issuetypes = response.json()
+        print(f"Retrieved {len(issuetypes)} issue types successfully.")
+        for issuetype in issuetypes:
+            print(f"Issue type: {issuetype['name']}")
+    else:
+        print(f"Failed to list issue types. Response: {response.text}")
+
+def test_issue_type_detail(issuetype_id):
+    url = f"{BASE_URL}/issuetypes/{issuetype_id}/"
+    response = requests.get(url)
+    print(f"Issue type detail status code: {response.status_code}")
+    if response.status_code == 200:
+        issuetype = response.json()
+        print(f"Retrieved issue type details: ID - {issuetype['issuetype_id']}, Name - {issuetype['name']}")
+    else:
+        print(f"Failed to retrieve issue type detail. Response: {response.text}")
+
+def test_delete_issue_type(issuetype_id):
+    url = f"{BASE_URL}/issuetypes/{issuetype_id}/delete/"
+    response = requests.delete(url)
+    print(f"Delete issue type status code: {response.status_code}")
+    if response.status_code == 204:
+        print(f"Issue type {issuetype_id} deleted successfully.")
+    else:
+        print(f"Failed to delete issue type {issuetype_id}. Response: {response.text}")
+
 # Testando os endpoints do Jira
 jira_domain = "stone-puc.atlassian.net"
 project_key = "CSTONE"
@@ -78,15 +109,15 @@ jira_email = "gabrielmmendes19@gmail.com"
 jira_api_token = "ATATT3xFfGF0xJ__SquSx3bKWcZ4dqWJyOS_MUVkZYTYY7v21dbfiptBvldgNnfYV-EwEim5385HhVlffiS4BgX1NiPYE5bsM8uXYfdO4fiyYIZZhE6hWcNmE2QLJQk6AX_XkUuHW1Xj2vGc97hfSRgejv21NVczaftxtqlQ_c-qdYSCetzZN6M=A80BA930"
 issuetypes = ["Sub-task","Story","Task"]
 start_date = "2024-04-01"
-end_date = "2024-09-05"
+end_date = "2024-09-18"
 issue_id = 10435
 project_id = 3
+issuetype_id = 10016
 
 
 # Testando os endpoints do Jira
-print("Testing fetch issue types...")
-test_fetch_issue_types(jira_domain, jira_email, jira_api_token)
 
+# Rotas das Issues
 print("\nTesting collect issues...")
 test_collect_issues(jira_domain, project_key, jira_email, jira_api_token, issuetypes=issuetypes, start_date=start_date, end_date=end_date)
 
@@ -98,3 +129,16 @@ test_issue_detail(issue_id)
 
 print(f"\nTesting delete issue for issue ID {issue_id}...")
 test_delete_issue(issue_id)
+
+# Rotas dos Tipos de Issues
+print("Testing collect issue types...")
+test_collect_issue_types(jira_domain, jira_email, jira_api_token)
+
+print("\nTesting list issue types...")
+test_list_issue_types()
+
+print(f"\nTesting issue type detail for issue type ID {issuetype_id}...")
+test_issue_type_detail(issuetype_id)
+
+print(f"\nTesting delete issue type for issue type ID {issuetype_id}...")
+test_delete_issue_type(issuetype_id)
