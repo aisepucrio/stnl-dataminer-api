ebf6989d7234cbb666577b1ecfa2eef73f7d9bf6
GabrielMM47 <gabrielmmendes@outlook.com>
2024-09-18 13:48:05 -0300

    feat: Adicionando serializer para os issuetypes.
    JIRA: [CSTONE-319][CSTONE-331]

--- jira/serializers.py
+++ jira/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import JiraIssue
+from .models import JiraIssue, JiraIssueType
 
 class JiraIssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = JiraIssue
+        fields = '__all__'
+
+class JiraIssueTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JiraIssueType
         fields = '__all__'
