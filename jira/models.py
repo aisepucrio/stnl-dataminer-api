7b42bedad24c62559a26c817f18a24ebfa57bced
GabrielMM47 <gabrielmmendes@outlook.com>
2024-10-01 15:30:57 -0300

    Definindo chaves primárias compostas para os modelos das issues e issuetypes no banco de dados.
    JIRA: [CSTONE-319]

--- jira/models.py
+++ jira/models.py
@@ -7,13 +7,17 @@ class JiraIssueType(models.Model):
     description = models.TextField(null=True, blank=True)
 
     class Meta:
-        unique_together = ('issuetype_id', 'name', 'domain')
+        constraints = [
+            models.UniqueConstraint(fields=['issuetype_id', 'domain'], name='unique_issuetype')
+        ]
+        # Definindo uma chave composta com `issuetype_id` e `domain`
+        unique_together = (('issuetype_id', 'domain'),) 
 
     def __str__(self):
         return f"{self.domain}: {self.name} ({self.id})"
 
 class JiraIssue(models.Model):
-    issue_id = models.CharField(max_length=100, unique=True)
+    issue_id = models.CharField(max_length=100, unique=True, primary_key=True)
     key = models.CharField(max_length=100)
     issuetype = models.CharField(max_length=100)
     summary = models.TextField()
@@ -27,6 +31,13 @@ class JiraIssue(models.Model):
     assignee = models.CharField(max_length=100, null=True, blank=True)
     reporter = models.CharField(max_length=100, null=True, blank=True)
     all_fields = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['issue_id', 'project'], name='unique_issue')
+        ]
+        # Definindo uma chave composta com `issue_id` e `project`
+        unique_together = (('issue_id', 'project'),)
 
     def __str__(self):
         return f"{self.key} - {self.summary}"
