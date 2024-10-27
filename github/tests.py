6c8f1b9346767f5cb9de45a760c6cba434787e11
brenonevs <brenobraganevs@gmail.com>
2024-10-27 00:23:26 -0300

    feat: Adicionando uso do .env pelo Docker
    JIRA: [CSTONE-320][CSTONE-368]

--- github/tests.py
+++ github/tests.py
@@ -5,13 +5,12 @@ from rest_framework import status
 class GitHubAPITests(APITestCase):
     def setUp(self):
         self.client = APIClient()
-        self.repo_name = "esp8266/Arduino"
-        self.start_date = "2024-07-20T00:00:00Z"
+        self.repo_name = "grafana/grafana"
+        self.start_date = "2023-07-20T00:00:00Z"
         self.end_date = "2024-08-31T23:59:59Z"
         
         self.commits_url = reverse('commit-list')
         self.issues_url = reverse('github-issue-list')  
-        print(f'Issues URL: {self.issues_url}')
         self.pull_requests_url = reverse('pullrequest-list')
         self.branches_url = reverse('branch-list')
 
