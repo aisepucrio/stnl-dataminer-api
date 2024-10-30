a1a88f1f55fc703e2c4d83672d6a2fa085289113
GabrielMM47 <116840873+GabrielMM47@users.noreply.github.com>
2024-10-30 18:47:59 -0300

    feat: Adicionando endpoint para verificar status das tasks
    JIRA: [CSTONE-320][CSTONE-370]

--- github/urls.py
+++ github/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GitHubCommitViewSet, GitHubIssueViewSet, GitHubPullRequestViewSet, GitHubBranchViewSet
+from .views import GitHubCommitViewSet, GitHubIssueViewSet, GitHubPullRequestViewSet, GitHubBranchViewSet, task_status
 
 router = DefaultRouter()
 router.register(r'commits', GitHubCommitViewSet, basename='commit')
-router.register(r'issues', GitHubIssueViewSet, basename='github-issue') 
+router.register(r'issues', GitHubIssueViewSet, basename='github-issue')
 router.register(r'pull-requests', GitHubPullRequestViewSet, basename='pullrequest')
 router.register(r'branches', GitHubBranchViewSet, basename='branch')
 
 urlpatterns = [
     path('github/', include(router.urls)),
+    path('tasks/<str:task_id>/status/', task_status, name='task_status'), 
 ]
