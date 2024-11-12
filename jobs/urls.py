b69197b2d317964ba201c4c861ca4c9ef63fd812
GabrielMM47 <gabrielmmendes@outlook.com>
2024-11-12 16:24:07 -0300

    test: Alterações nos testes

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TaskStatusView, TaskCancelView
 
 urlpatterns = [
-    path('task/<str:task_id>/', TaskStatusView.as_view(), name='task-status'),
-    path('task/<str:task_id>/cancel/', TaskCancelView.as_view(), name='task-cancel'),
+    path('<str:task_id>/', TaskStatusView.as_view(), name='task-status'),
+    path('<str:task_id>/cancel/', TaskCancelView.as_view(), name='task-cancel'),
 ]
