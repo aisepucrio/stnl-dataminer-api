19e7920181c98459313608e2d44d39dfd09faff1
brenonevs <brenobraganevs@gmail.com>
2024-11-14 14:22:33 -0300

    fix: Retirando parte das features
    JIRA: [CSTONE-320][CSTONE-401]

--- dataminer_api/urls.py
+++ dataminer_api/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
-from features import views as features_views 
 
 def home(request):
     return HttpResponse("Bem-vindo à página inicial do DataMiner API!!!!!!!")
@@ -11,6 +10,5 @@ urlpatterns = [
     path('api/', include('github.urls')),
     path('jira/', include('jira.urls')),
     path('', home),  # Adiciona uma página inicial para a URL raiz
-    path('minerar/', features_views.minerar_features_view, name='minerar_features'),
     path('api/jobs/', include('jobs.urls'))
 ]
