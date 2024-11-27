08c4d60f84710bc5700dcdf8323c839ba3f39cfa
Daniel Coutinho <danieljose.bc@gmail.com>
2024-11-27 12:09:48 -0300

    Ajustes conexão com o banco.

--- dataminer_api/settings.py
+++ dataminer_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +78,12 @@ WSGI_APPLICATION = 'dataminer_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mining_db',
-        'USER': 'aise-stone',
-        'PASSWORD': 'senha',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    },
 }
 
 # Password validation
