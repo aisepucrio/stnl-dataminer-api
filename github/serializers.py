ee6ad447d4c3a34f179feaf1fedc3cdffa1dbba5
brenonevs <brenobraganevs@gmail.com>
2025-01-08 12:32:58 -0300

    feat: Começando a implementação de Generic Views para criação de rotas de consulta ao banco
    JIRA: [CSTONE-320][CSTONE-452]

--- github/serializers.py
+++ github/serializers.py
@@ -1,20 +1,63 @@
 from rest_framework import serializers
-from .models import GitHubCommit, GitHubIssue, GitHubPullRequest, GitHubBranch
+from .models import GitHubCommit, GitHubIssue, GitHubPullRequest, GitHubBranch, GitHubAuthor, GitHubModifiedFile, GitHubMethod
+
+class GitHubAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GitHubAuthor
+        fields = ['name', 'email']
+
+class GitHubMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GitHubMethod
+        fields = ['name', 'complexity', 'max_nesting']
+
+class GitHubModifiedFileSerializer(serializers.ModelSerializer):
+    methods = GitHubMethodSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = GitHubModifiedFile
+        fields = ['filename', 'change_type', 'added_lines', 'deleted_lines', 'complexity', 'methods']
 
 class GitHubCommitSerializer(serializers.ModelSerializer):
+    author = GitHubAuthorSerializer(read_only=True)
+    committer = GitHubAuthorSerializer(read_only=True)
+    modified_files = GitHubModifiedFileSerializer(many=True, read_only=True)
+    
     class Meta:
         model = GitHubCommit
         fields = '__all__'
 
 class GitHubIssueSerializer(serializers.ModelSerializer):
+    created_at_formatted = serializers.SerializerMethodField()
+    updated_at_formatted = serializers.SerializerMethodField()
+
     class Meta:
         model = GitHubIssue
         fields = '__all__'
 
+    def get_created_at_formatted(self, obj):
+        return obj.created_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_updated_at_formatted(self, obj):
+        return obj.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+
 class GitHubPullRequestSerializer(serializers.ModelSerializer):
+    created_at_formatted = serializers.SerializerMethodField()
+    updated_at_formatted = serializers.SerializerMethodField()
+    labels_list = serializers.SerializerMethodField()
+
     class Meta:
         model = GitHubPullRequest
         fields = '__all__'
+
+    def get_created_at_formatted(self, obj):
+        return obj.created_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_updated_at_formatted(self, obj):
+        return obj.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_labels_list(self, obj):
+        return obj.labels if isinstance(obj.labels, list) else []
 
 class GitHubBranchSerializer(serializers.ModelSerializer):
     class Meta:
--- github/views.py
+++ github/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from rest_framework import status
 from celery.result import AsyncResult
 from jobs.tasks import fetch_commits, fetch_issues, fetch_pull_requests, fetch_branches
+from .models import GitHubCommit, GitHubIssue, GitHubPullRequest, GitHubBranch
+from .serializers import GitHubCommitSerializer, GitHubIssueSerializer, GitHubPullRequestSerializer, GitHubBranchSerializer
 
 class GitHubCommitViewSet(viewsets.ViewSet):
     def list(self, request):
@@ -54,3 +56,44 @@ class GitHubBranchViewSet(viewsets.ViewSet):
         return Response({
             "task_id": task.id
         }, status=status.HTTP_200_OK)
+
+# Novas views genéricas para consulta
+class CommitListView(generics.ListAPIView):
+    queryset = GitHubCommit.objects.all()
+    serializer_class = GitHubCommitSerializer
+    filterset_fields = ['repository', 'author', 'date']
+
+class CommitDetailView(generics.RetrieveAPIView):
+    queryset = GitHubCommit.objects.all()
+    serializer_class = GitHubCommitSerializer
+    lookup_field = 'sha'
+
+class IssueListView(generics.ListAPIView):
+    queryset = GitHubIssue.objects.all()
+    serializer_class = GitHubIssueSerializer
+    filterset_fields = ['repository', 'state', 'creator']
+
+class IssueDetailView(generics.RetrieveAPIView):
+    queryset = GitHubIssue.objects.all()
+    serializer_class = GitHubIssueSerializer
+    lookup_field = 'issue_id'
+
+class PullRequestListView(generics.ListAPIView):
+    queryset = GitHubPullRequest.objects.all()
+    serializer_class = GitHubPullRequestSerializer
+    filterset_fields = ['repository', 'state', 'creator']
+
+class PullRequestDetailView(generics.RetrieveAPIView):
+    queryset = GitHubPullRequest.objects.all()
+    serializer_class = GitHubPullRequestSerializer
+    lookup_field = 'pr_id'
+
+class BranchListView(generics.ListAPIView):
+    queryset = GitHubBranch.objects.all()
+    serializer_class = GitHubBranchSerializer
+    filterset_fields = ['repository', 'name']
+
+class BranchDetailView(generics.RetrieveAPIView):
+    queryset = GitHubBranch.objects.all()
+    serializer_class = GitHubBranchSerializer
+    lookup_field = 'name'
