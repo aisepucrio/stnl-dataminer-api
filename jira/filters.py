9c1171372f2582af4c3c5677ff64485097941b56
GabrielMM47 <gabrielmmendes@outlook.com>
2025-01-17 05:26:22 -0300

    Adição dos filtros e padronização do retorno entre a parte do Jira e do GitHub quando uma task é iniciada.

---
+++ jira/filters.py
@@ -0,0 +1,23 @@
+from django_filters import rest_framework as filters
+from .models import JiraIssue
+
+class JiraIssueFilter(filters.FilterSet):
+    # Filtros de datas
+    created_after = filters.DateTimeFilter(field_name='created', lookup_expr='gte')
+    created_before = filters.DateTimeFilter(field_name='created', lookup_expr='lte')
+    updated_after = filters.DateTimeFilter(field_name='updated', lookup_expr='gte')
+    updated_before = filters.DateTimeFilter(field_name='updated', lookup_expr='lte')
+    
+    # Filtros de texto
+    summary = filters.CharFilter(lookup_expr='icontains')
+    description = filters.CharFilter(lookup_expr='icontains')
+    creator = filters.CharFilter(lookup_expr='icontains')
+    assignee = filters.CharFilter(lookup_expr='icontains')
+    status = filters.CharFilter(lookup_expr='iexact')
+    project = filters.CharFilter(lookup_expr='iexact')
+    priority = filters.CharFilter(lookup_expr='iexact')
+    issuetype = filters.CharFilter(lookup_expr='iexact')
+
+    class Meta:
+        model = JiraIssue
+        fields = ['issue_id', 'project', 'status', 'priority', 'issuetype']
