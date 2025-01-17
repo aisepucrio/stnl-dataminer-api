c6a41ad9ae962337dccb79ddcd70d7ac376ce0e1
GabrielMM47 <gabrielmmendes@outlook.com>
2025-01-17 05:26:22 -0300

    Adição dos filtros e padronização do retorno entre a parte do Jira e do GitHub quando uma task é iniciada.

--- jira/urls.py
+++ jira/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IssueCollectView, IssueListView, IssueDetailView
+from .views import JiraIssueCollectView, JiraIssueListView, JiraIssueDetailView
 
 urlpatterns = [
-    path('issues/collect/', IssueCollectView.as_view(), name='collect-jira-issues'),
-    path('issues/', IssueListView.as_view(), name='issues-list'),
-    path('issues/<str:issue_key>/', IssueDetailView.as_view(), name='issue-detail')
+    path('issues/collect/', JiraIssueCollectView.as_view(), name='collect-jira-issues'),
+    path('issues/', JiraIssueListView.as_view(), name='issues-list'),
+    path('issues/<str:issue_key>/', JiraIssueDetailView.as_view(), name='issue-detail')
 ]
