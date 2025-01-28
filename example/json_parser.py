588c918f4cb0efd2d68aeaa320c90f7a2f3c80c4
brenonevs <brenobraganevs@gmail.com>
2025-01-28 13:41:44 -0300

    [logs] Melhorando o log das PRs para ficar mais bonito e parecido com o de Issues

--- example/json_parser.py
+++ example/json_parser.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     file_path = "esp8266_Arduino_issues.json"
     data = read_json(file_path)
 
+    print(f'Total de issues: {len(data)}')
+    
     for issue in data:
         print(f'{issue["number"]} - {issue["created_at"]}')
 
