862c88e9fcffcb12aa0b8b2d04db0900634b9e44
Daniel Coutinho <danieljose.bc@gmail.com>
2025-05-26 17:06:57 -0300

    [fix] fix urls.py

--- dataminer_api/urls.py
+++ dataminer_api/urls.py
@@ -12,12 +12,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/github/', include('github.urls', namespace='github')),
     path('api/jira/', include('jira.urls')),
     path('api/jobs/', include('jobs.urls'))
-]
-
-urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
