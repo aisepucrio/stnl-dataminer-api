2bf1ae3d1270f9f506cc0710f6745cb8363d2d92
Daniel Coutinho <danieljose.bc@gmail.com>
2025-06-04 14:11:01 -0300

    [feat] register all models with admin panel

--- jira/admin.py
+++ jira/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import (
+    JiraIssue, JiraProject, JiraUser, JiraComment, JiraChecklist,
+    JiraIssueType, JiraSprint, JiraIssueLink, JiraCommit,
+    JiraActivityLog, JiraHistory, JiraHistoryItem
+)
 
 # Register your models here.
+admin.site.register(JiraIssue)
+admin.site.register(JiraProject)
+admin.site.register(JiraUser)
+admin.site.register(JiraComment)
+admin.site.register(JiraChecklist)
+admin.site.register(JiraIssueType)
+admin.site.register(JiraSprint)
+admin.site.register(JiraIssueLink)
+admin.site.register(JiraCommit)
+admin.site.register(JiraActivityLog)
+admin.site.register(JiraHistory)
+admin.site.register(JiraHistoryItem)
+
