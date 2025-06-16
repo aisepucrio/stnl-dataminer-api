1916fb51d9c752b656289a89e4481a4f9b4d77a8
Ole Remi Dahl <olerd@stud.ntnu.no>
2025-06-16 15:56:41 -0300

    faked migration

--- stackoverflow/migrations/0017_alter_stackanswer_time_mined_and_more.py
+++ stackoverflow/migrations/0017_alter_stackanswer_time_mined_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('stackoverflow', '0016_stackuser_time_mined_alter_stackanswer_time_mined_and_more'),
+        ('stackoverflow', '0005_alter_stackanswer_time_mined_and_more'),
     ]
 
     operations = [
