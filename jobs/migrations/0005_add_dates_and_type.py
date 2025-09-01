531b553a0b79c7cf7216b6c33306f2b5cf35acc9
Robbie Carvalho <130287531+R0bbyt3@users.noreply.github.com>
2025-09-01 10:24:49 -0300

    Mudando campos da tabela jobs

---
+++ jobs/migrations/0005_add_dates_and_type.py
@@ -0,0 +1,31 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("jobs", "0004_auto_20250819_1952"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="task",
+            name="date_init",
+            field=models.DateTimeField(),
+        ),
+        migrations.AddField(
+            model_name="task",
+            name="date_end",
+            field=models.DateTimeField(),
+        ),
+        migrations.AddField(
+            model_name="task",
+            name="date_last_update",
+            field=models.DateTimeField(),
+        ),
+        migrations.AddField(
+            model_name="task",
+            name="type",
+            field=models.CharField(max_length=100),
+        ),
+    ]
